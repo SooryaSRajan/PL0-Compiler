ae5740bafe621faf890ca52c0041fd26e3a3023c
sooryasrajan <soorya.s27@gmail.com>
2023-01-03 01:39:39 +0530

    Changed file extension

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        CharStream codePointCharStream = CharStreams.fromFileName("src/InputFiles/stack");
+        CharStream codePointCharStream = CharStreams.fromFileName("src/InputFiles/stack.pl0");
 
         //generates a lexer output for the input code
         PL0Lexer lexer = new PL0Lexer(codePointCharStream);
